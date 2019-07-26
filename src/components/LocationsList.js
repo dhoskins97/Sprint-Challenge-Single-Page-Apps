0edbba2ec05bbe7d5dca7c074b574209d198684d
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-07-26 12:57:05 -0400

    locations tab functional

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import LocationCard from './LocationCard';
 
 export default function LocationsList() {
 
+const [locations, setLocations] = useState([]);
+
+useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/location/`).then( (res) => {
+      setLocations(res.data.results);
+    })
+  }, [])
+
+  return (
+      <section>
+          {locations.map( location => {
+              return <LocationCard name={location.name} type={location.type} dimension={location.dimension} residents={location.residents.length} />
+          })}
+      </section>
+  )
 }
